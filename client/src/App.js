d3e8910045d257f40dbc0813885c303e8d2d274f
Rihhard Elm <elm.rihhard@gmail.com>
2023-04-19 16:57:19 +0300

    start with frontend CRUD and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+const API_BASE = 'http://localhost:3000';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([]);
+	const [popup, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState('');
+
+	useEffect(() => {
+		GetTodos();
+	}, []);
+
+	const GetTodos = () => {
+		fetch(API_BASE + '/todos')
+			.then((res) => res.json())
+			.then((data) => {
+				setTodos(data);
+			})
+			.catch((err) => {
+				console.error('Error: ', err);
+			})
+
+	}
+	if (typeof window !== 'undefined') { 
+		console.log(todos);
+	 }
+	return (
+		<div className="App">
+			<h1>Hi, $User</h1>
+			<h4>Your Tasks</h4>
+			<div className=" todos">
+				<div className="todo">
+					<div className="checkbox"></div>
+
+					<div className="text">File your taxes</div> 
+
+					<div className="delete-todo">X</div>
+				</div>
+
+				<div className="todo is-complete">
+					<div className="checkbox"></div>
+					
+					<div className="text">Go to the dentist</div> 
+
+					<div className="delete-todo">X</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
